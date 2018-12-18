944226f938cbf4c759f0aadbb4f92cbd381e3bcf
WisdomWang <294814110@qq.com>
2018-12-18 16:03:37 +0800

    Initial Commit

---
+++ Wallpaper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wallpaper
+//
+//  Created by Cary on 2018/12/18.
+//  Copyright © 2018 Cary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
