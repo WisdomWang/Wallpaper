5b693d246ede821165aacb4023ff604d28945f18
WisdomWang <294814110@qq.com>
2018-12-29 18:01:32 +0800

    first commit

---
+++ Wallpaper/MainCell.swift
@@ -0,0 +1,13 @@
+//
+//  MainCell.swift
+//  Wallpaper
+//
+//  Created by Cary on 2018/12/28.
+//  Copyright © 2018 Cary. All rights reserved.
+//
+
+import UIKit
+
+class MainCell: UICollectionViewCell {
+    
+}
---
+++ Wallpaper/mainModel.swift
@@ -0,0 +1,13 @@
+//
+//  mainModel.swift
+//  Wallpaper
+//
+//  Created by Cary on 2018/12/29.
+//  Copyright © 2018 Cary. All rights reserved.
+//
+
+import UIKit
+
+class mainModel: NSObject {
+
+}
